dc0b8b139fc87159fbf6d141defb70954f6a4e93
verno <verno@ASUS>
2026-03-05 21:57:59 -0600

    ej 13

--- Actividad 16 ej 11 al 20/Program.cs
+++ Actividad 16 ej 11 al 20/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Actividad 11 ej 11 al 20");
+Console.WriteLine("Ej ");
+Console.WriteLine("Elija una opción:");
+int opcion = int.Parse(Console.ReadLine());
+
+switch (opcion)
+{
+    case 11:
+        Console.WriteLine("Ejercicio 11");
+        int total11 = 0;
+        int i11=1;
+        do
+        {
+            Console.WriteLine("Ingrese un numero");
+            int num1 = int.Parse(Console.ReadLine());
+            total11 = total11 + num1;
+            i11++;
+        }
+        while (i11 <= 5);
+        Console.WriteLine($"El total es {total11} ");
+        break;
+    case 12:
+        Console.WriteLine("Ejercicio 12");
+        int total12 = 0;
+        int num12;
+        do
+        {
+            Console.WriteLine("Ingrese un numero");
+           num12 = int.Parse(Console.ReadLine());
+            total12 = total12 + num12;
+        }
+        while (num12 !=0);
+        Console.WriteLine($"El total es {total12} ");
+        break;
+    case 13:
+        Console.WriteLine("Ejercicio 13");
+
+
+}
